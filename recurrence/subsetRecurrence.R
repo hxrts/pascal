f22f705282b8d0c7f2a145a09244073a037d250c
hxrts <bermans@mskcc.org>
2015-12-15 12:31:42 -0500

    better pdf naming

--- recurrence/subsetRecurrence.R
+++ recurrence/subsetRecurrence.R
@@ -27,16 +27,16 @@ buildD<-function(samples){
 }
 
 # draw heatmap
-heatM<-function(D,file){
+heatM<-function(D,name){
 	colorRamp<-colorRampPalette(c("white","blue"))(max(D)+1)
 	matrixpar=list(mfrow=c(2,1),mar=c(1,3,1,1),oma=c(2,2,2,2))  # mar/oma - bottom, left, top, right
-	pdf(file)
+	pdf(paste("recurrent_mutations/",name,"_recurrent.pdf",sep=""))
 		par(matrixpar)
 		layout(matrix(1:2,ncol=2), widths=c(11,1), heights=c(1,1))
 
 		# plot
 		image(1:ncol(D),1:nrow(D),z=t(as.matrix(D[nrow(D):1,])),col=colorRamp,xlab="",ylab="",axes=FALSE)
-		title(main=unlist(strsplit(file,".pdf")),line=0.5,cex.main=0.8)
+		title(main=name,line=0.5,cex.main=0.8)
 		axis(1,at=ncol(D):1,labels=names(D),cex.axis=0.6,las=2)			# x-axis
 		axis(2,at=nrow(D):1,labels=row.names(D),cex.axis=0.45,las=1)	# y-axis
 		box("plot")
@@ -66,7 +66,7 @@ for (subnum in 1:nrow(subsets)){
 	write.table(muts,file=paste("recurrent_mutations/",name,"_recurrent.txt",sep=""),quote=FALSE,sep="\t",row.names=FALSE)
 
 	heat<-buildD(subset)
-	heatM(heat,paste("recurrent_mutations/",name,"_recurrent.pdf",sep=""))
+	heatM(heat,name)
 
 }
 
