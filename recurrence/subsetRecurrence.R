fa1c3512ff5bf50b98e5699075fd7eb7ac240b86
hxrts <bermans@mskcc.org>
2015-12-15 13:03:55 -0500

    adds grid & margin

--- recurrence/subsetRecurrence.R
+++ recurrence/subsetRecurrence.R
@@ -29,7 +29,7 @@ buildD<-function(samples){
 # draw heatmap
 heatM<-function(D,name){
 	colorRamp<-colorRampPalette(c("white","blue"))(max(D)+1)
-	matrixpar=list(mfrow=c(2,1),mar=c(1,3,1,1),oma=c(2,2,2,2))  # mar/oma - bottom, left, top, right
+	matrixpar=list(mfrow=c(2,1),mar=c(2,3,1,1),oma=c(2,2,2,2))  # mar/oma - bottom, left, top, right
 	pdf(paste("recurrent_mutations/",name,"_recurrent.pdf",sep=""))
 		par(matrixpar)
 		layout(matrix(1:2,ncol=2), widths=c(11,1), heights=c(1,1))
@@ -39,6 +39,7 @@ heatM<-function(D,name){
 		title(main=name,line=0.5,cex.main=0.8)
 		axis(1,at=ncol(D):1,labels=names(D),cex.axis=0.6,las=2)			# x-axis
 		axis(2,at=nrow(D):1,labels=row.names(D),cex.axis=0.45,las=1)	# y-axis
+		grid(nx=ncol(D),ny=nrow(D),col="gray92",lty=1,lwd=par("lwd"),equilogs=TRUE)
 		box("plot")
 
 		# legend
@@ -46,6 +47,7 @@ heatM<-function(D,name){
 		colorLevels<-min(D):max(D)
 		image(1,colorLevels,matrix(data=colorLevels,ncol=length(colorLevels),nrow=1),col=colorRamp,xlab="",ylab="",xaxt="n",axes=FALSE)
 		axis(2,at=min(D):max(D),labels=min(D):max(D),las=1,cex.axis=0.6)
+		grid(nx=ncol(D),ny=nrow(D),col="gray92",lty=1,lwd=par("lwd"),equilogs=TRUE)
 		box("plot")
 
 	dev.off()
