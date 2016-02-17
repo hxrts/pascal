e32bfd0ea09ca1848973fc9c7417f9eee45aedd2
hxrts <bermans@mskcc.org>
2016-02-17 12:42:24 -0500

    init script update

--- lib/init.R
+++ lib/init.R
@@ -8,6 +8,19 @@ suppressMessages(pacman::p_load(dplyr,readr,tidyr,magrittr,purrr,stringr,rlist,c
 # input
 #------
 
+# read samples file
+samples <-
+	read.delim("sample_sets.txt",sep=" ",stringsAsFactors=FALSE,header=FALSE) %>%
+	filter(!grepl("#",V1)) %>%
+	(function(sets){
+		tumor <-
+			apply(sets,1, function(row) row %>% list.filter (.!="") %>% head(-1)) %>% unlist
+		normal <-
+			apply(sets,1, function(row) row %>% list.filter (.!="") %>% tail(1)) %>%
+			rep(apply(sets,1,function(row) row %>% list.filter(.!="") %>% length-1))
+		data.frame(tumor,normal,stringsAsFactors=FALSE) %>% tbl_df
+	})
+
 # read subsets file
 subsets <-
 	if("subsets.txt" %>% file.exists){
@@ -21,24 +34,12 @@ subsets <-
 		setNames(.[1,]) %>%
 		slice(-1) %>%
 		tbl_df
-}
-
-# read samples file
-samples <-
-	read.delim("sample_sets.txt",sep=" ",stringsAsFactors=FALSE,header=FALSE) %>%
-	setNames(c("patient",1:(ncol(.)-1))) %>%
-	group_by(patient) %>%
-	filter(!grepl("#",patient)) %>%
-	summarise_each(funs(ifelse(.=="","NA",.))) %>%
-	setNames(c("tumor","normal"))
-
-# add all samples as subset if no subsets file exists
-if("subsets" %>% exists){
-	subsets <-
+	} else {	# add all samples as subset if no subsets file exists
 		samples$tumor %>%
 		as.data.frame %>%
 		setNames(getwd() %>% strsplit("/") %>% unlist %>% tail(1))
-}
+	}
+
 
 # filter samples as those in subsets
 samples %<>% filter(tumor %in% unlist(subsets))
